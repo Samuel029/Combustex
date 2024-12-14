a5fe19ec44c6b488684849fd58200fc056d8ded2
Felipe Pereira <felipepereirabritto08@gmail.com>
2024-12-14 20:16:56 -0300

    teste

---
+++ manter_ativos.py
@@ -0,0 +1,16 @@
+import time
+import requests
+
+sites = [
+    "https://conversorpdfeword.onrender.com/",
+    "https://combustex.onrender.com/"
+]
+
+def manter_sites_ativos():
+    while True:
+        for site in sites:
+            requests.get(site, timeout=10)
+            time.sleep(300)
+
+if __name__ == "__main__":
+    manter_sites_ativos()
